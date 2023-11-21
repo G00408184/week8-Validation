9395941a0afc3cab462bed9ae23a7332f3acbea2
Jakub Mamala <G00408184@atu.ie>
2023-11-21 15:32:12 +0000

    project created

---
+++ src/main/java/ie/atu/week8validation/Week8ValidationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week8validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week8ValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week8ValidationApplication.class, args);
+    }
+
+}
