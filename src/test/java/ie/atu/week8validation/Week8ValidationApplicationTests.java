0a643edcbfe043de0ace2146a5efa50ba450118e
Jakub Mamala <G00408184@atu.ie>
2023-11-21 16:01:17 +0000

    prepared the service class for future lab

---
+++ src/test/java/ie/atu/week8validation/Week8ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week8validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week8ValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
